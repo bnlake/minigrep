359c2a4af6fda4de81c906d9545756d93ff12193
Brian Lake <bnlake@gmail.com>
2023-04-03 17:05:53 -0500

    return result from run command; error handle run in main process

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::{env, fs, process};
 
 fn main() {
@@ -11,7 +12,10 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Error running application: {}", e);
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -30,8 +34,8 @@ impl Config {
     }
 }
 
-fn run(config: Config) {
-    let content = fs::read_to_string(config.file_path).expect("Should have been able to find file");
-
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.file_path)?;
     println!("With text:\n{}", content);
+    Ok(())
 }
