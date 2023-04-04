d5f544a2d9a5f429b6ae2e7830a793078b362993
Brian Lake <bnlake@gmail.com>
2023-04-04 07:34:13 -0500

    search function passing test

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,14 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 #[cfg(test)]
@@ -38,6 +45,6 @@ mod tests {
 Rust:
 safe, fast, productive.
 Pick three.";
-        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
